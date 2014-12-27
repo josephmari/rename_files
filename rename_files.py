f361932cb7374d81ebe62f7acf52ba7bbacbfd28
josephmari <joseph.mari@gmail.com>
2014-12-26 22:42:48 -0500

    List files
    List files using os.listdir

---
+++ rename_files.py
@@ -0,0 +1,11 @@
+import os
+
+def rename_files():
+    #(1) get file names from a folder
+    file_list = os.listdir(r"/Users/josephmari/Documents/Python Programming/Intro to Python/rename_files/prank")
+    print(file_list)
+    
+    
+    #(2) rename files
+
+rename_files()
